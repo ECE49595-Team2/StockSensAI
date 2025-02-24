20f4c7818fd0fa87afb972aea3f6e99984635fb1
kevastator <kevastator1@gmail.com>
2025-02-24 17:50:38 -0500

    Full News Analysis Endpoint

--- backend/llm-api/include/api_helper.h
+++ backend/llm-api/include/api_helper.h
@@ -5,20 +5,27 @@
 #include <vector>
 #include <curl/curl.h>
 
-#include <iostream>
-
 #include <nlohmann/json.hpp>
 #include <dotenv.h>
 
-#define OPEN_ROUTER_ENDPOINT = "https://openrouter.ai/api/v1/chat/completions";
-
 using json = nlohmann::json;
 
+// Define a constant endpoints
+const std::string OPEN_ROUTER_ENDPOINT = "https://openrouter.ai/api/v1/chat/completions";
+const std::string POLYGON_ENDPOINT = "https://api.polygon.io/v2";
+
+// Define a function to handle writing callbacks for CURL
 size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *userp);
 
+// Define a function to call external APIs
 std::string callExternalApiPost(const std::string& url, const std::string& method = "GET", const std::string& body = "", const std::string& bearerKey = "");
 
-json getPolygonNews(const std::string& ticker);
+// Define helper functions
+json getPolygonNews(const std::string& ticker, const double ageLim = 0);
 json getLlamaPong();
+json getLlamaPrompt(const std::string& prompt);
+
+// News analysis function
+json getNewsAnalysis(const std::string& ticker, const double ageLim = 0);
 
 #endif
--- backend/llm-api/src/server_main.cpp
+++ backend/llm-api/src/server_main.cpp
@@ -13,99 +13,200 @@
 
 using json = nlohmann::json;
 
-#define LISTEN_QUEUE 50 /* Max outstanding connection requests; listen() param */
+const int LISTEN_QUEUE_SIZE = 50;
+const int BUFFER_SIZE = 2048;
+
+void handle_get_request(HTTPMessage headerData, HTTPResponse* response)
+{
+    if (headerData.path == "/ping")
+    {
+        // Test GET pong response
+        testGetPong(response);
+    }
+    else if (headerData.path == "/testEx")
+    {
+        // Call an external API
+        std::string apiRes = callExternalApiPost("https://httpbin.org/get");
+
+        // Send a generic response with the API result
+        genericResponse(response, 200, JSON, apiRes);
+    }
+    else if (headerData.path == "/testOpenRouterApi")
+    {
+        json responseJson = getLlamaPong();
+
+        genericResponse(response, 200, JSON, responseJson.dump());
+    }
+    else
+    {
+        badRequestResponse(response);
+    }
+}
+
+void handle_post_request(HTTPMessage headerData, HTTPResponse* response)
+{
+    if (headerData.path == "/testJson")
+    {
+        try
+        {
+            // Parse the request body as JSON
+            json requestJson = json::parse(headerData.body);
+
+            // Create a response JSON
+            json responseJson = {
+                {"status", "success"},
+                {"received", requestJson}
+            };
+
+            // Send a generic response with the response JSON
+            genericResponse(response, 200, JSON, responseJson.dump());
+        }
+        catch (const std::exception& e)
+        {
+            // If there is an error parsing the request body, send a bad request response
+            badRequestResponse(response);
+        }
+    }
+    else if (headerData.path == "/testJsonApi")
+    {
+        // Call an external API
+        std::string apiRes = callExternalApiPost("https://httpbin.org/get");
+
+        try
+        {
+            // Parse the response body as JSON
+            json responseJson = json::parse(apiRes);
+
+            // Create a response JSON
+            json resultJson = {
+                {"status", "success"},
+                {"origin-ip", responseJson["origin"].get<std::string>()}
+            };
+
+            // Send a generic response with the response JSON
+            genericResponse(response, 200, JSON, resultJson.dump());
+        }
+        catch (const std::exception& e)
+        {
+            // If there is an error parsing the response body, send a bad request response
+            std::cerr << "Error parsing JSON: " << e.what() << std::endl;
+            badRequestResponse(response);
+        }
+    }
+    else if (headerData.path == "/testPolygonApi")
+    {
+        try
+        {
+            // Get the ticker from the request body
+            json requestJson = json::parse(headerData.body);
+            std::string ticker = requestJson["ticker"].get<std::string>();
+
+            double timeLim = 0;
+
+            // Check if there is a time limit field in the request body
+            if (requestJson.find("time_limit") != requestJson.end())
+            {
+                timeLim = requestJson["time_limit"].get<double>();
+            }
+
+            json responseJson = getPolygonNews(ticker, timeLim);
+
+            genericResponse(response, 200, JSON, responseJson.dump());
+        }
+        catch (const std::exception& e)
+        {
+            std::cerr << "Error getting ticker: " << e.what() << std::endl;
+            badRequestResponse(response);
+        }
+    }
+    else if (headerData.path == "/testLlamaPrompt")
+    {
+        try
+        {
+            // Get the prompt from the request body
+            json requestJson = json::parse(headerData.body);
+            std::string prompt = requestJson["prompt"].get<std::string>();
+
+            std::cout << prompt << std::endl;
+
+            json responseJson = getLlamaPrompt(prompt);
+
+            genericResponse(response, 200, JSON, responseJson.dump());
+        }
+        catch (const std::exception& e)
+        {
+            std::cerr << "Error getting prompt: " << e.what() << std::endl;
+            badRequestResponse(response);
+        }
+    }
+    else if (headerData.path == "/newsAnalysis")
+    {
+        try
+        {
+            // Get the ticker from the request body
+            json requestJson = json::parse(headerData.body);
+            std::string ticker = requestJson["ticker"].get<std::string>();
+
+            double timeLim = 0;
+
+            // Check if there is a time limit field in the request body
+            if (requestJson.find("time_limit") != requestJson.end())
+            {
+                timeLim = requestJson["time_limit"].get<double>();
+            }
+
+            json responseJson = getNewsAnalysis(ticker, timeLim);
+
+            genericResponse(response, 200, JSON, responseJson.dump());
+        }
+        catch (const std::exception& e)
+        {
+            std::cerr << "Error getting analysis: " << e.what() << std::endl;
+            badRequestResponse(response);
+        }
+    }
+    else
+    {
+        badRequestResponse(response);
+    }
+}
 
 void handle_request(int client_sockfd)
 {
-    char buffer[2048];
+    // Receive request data from the client
+    char buffer[BUFFER_SIZE];
     int numbytesRec = recv(client_sockfd, buffer, sizeof(buffer), 0);
 
     if (numbytesRec < 0) {
-        perror("recv");
-        exit(1);
+        // Handle error receiving data
+        std::cerr << "Error receiving data" << std::endl;
+        return;
     }
 
     // Null-terminate the received data
     buffer[numbytesRec] = '\0';
 
+    // Convert received data to a string
     std::string request = std::string(buffer);
 
-    std::cout << "Recieved Request of length: " << numbytesRec << std::endl;
-    
+    // Print request length
+    std::cout << "\nRecieved Request of length: " << numbytesRec << std::endl;
+
     // Setup parsing
     HTTPMessage headerData;
     HTTPResponse response;
 
+    // Parse the HTTP message
     headerData = parseHTTPMessage(request);
 
+    // Handle different HTTP methods
     if (headerData.method == "GET")
     {
-        if (headerData.path == "/ping")
-        {
-            testGetPong(&response);
-        }
-        else if (headerData.path == "/testex")
-        {
-            std::string apiRes = callExternalApiPost("https://httpbin.org/get");
-            genericResponse(&response, 200, JSON, apiRes);
-        }
-        else if (headerData.path == "/testopenrouterapi")
-        {
-            json responseJson = getLlamaPong();
-
-            genericResponse(&response, 200, JSON, responseJson.dump());
-        }
-        else
-        {
-            badRequestResponse(&response);
-        }
+        handle_get_request(headerData, &response);
     }
     else if (headerData.method == "POST")
     {
-        if (headerData.path == "/testjson")
-        {
-            try
-            {
-                json requestJson = json::parse(headerData.body);
-
-                json responseJson = {
-                    {"status", "success"},
-                    {"received", requestJson}
-                };
-
-                genericResponse(&response, 200, JSON, responseJson.dump());
-            }
-            catch (const std::exception& e)
-            {
-                badRequestResponse(&response);
-            }
-        }
-        else if (headerData.path == "/testjsonapi")
-        {
-            json apiRes = json::parse(callExternalApiPost("https://httpbin.org/get"));
-
-            // Example extracting the ip from the test api field
-            json responseJson = {
-                {"status", "success"},
-                {"origin-ip", apiRes["origin"].get<std::string>()}
-            };
-
-            genericResponse(&response, 200, JSON, responseJson.dump());
-        }
-        else if (headerData.path == "/testpolygonapi")
-        {
-            std::string ticker = json::parse(headerData.body)["ticker"];
-
-            std::cout << ticker << std::endl;
-
-            json responseJson = getPolygonNews(ticker);
-
-            genericResponse(&response, 200, JSON, responseJson.dump());
-        }
-        else
-        {
-            badRequestResponse(&response);
-        }
+        handle_post_request(headerData, &response);
     }
     else if (headerData.method == "NULLFOR")
     {
@@ -116,48 +217,62 @@ void handle_request(int client_sockfd)
         methodNotAllowedResponse(&response);
     }
 
+    // Form the full HTTP response
     std::string strResponse = formHTTPFullResponse(response.code, response.contentType, response.body);
 
-    std::cout << strResponse << std::endl;
+    // Print the response
+    std::cout << strResponse << "\n" << std::endl;
 
-    send(client_sockfd, strResponse.c_str(), strResponse.length(), 0);
+    // Send the response back to the client
+    if (send(client_sockfd, strResponse.c_str(), strResponse.length(), 0) < 0)
+    {
+        std::cerr << "\nError sending response\n" << std::endl;
+    }
 }
 
 int main(int argc, char *argv[])
 {
+    // Validate the number of command line arguments
     if (argc != 2) {
         std::cout << "usage: ./llm_exec [server port]\n" << std::endl;
-        exit(1);
+        return 1;
     }
 
-    int servPortNumber = std::stoi(argv[1]); // convert to int
+    // Convert the server port number from string to integer
+    int servPortNumber = std::stoi(argv[1]);
+
+    // Declare socket variables
     int sockfd;
     int client_sockfd;
     struct sockaddr_in my_addr;
-    struct sockaddr_in their_addr; /* client's address info */
+    struct sockaddr_in their_addr;
     socklen_t sin_size;
+    
+    // Create socket
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
-    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-		perror("socket");
-		exit(1);
+    if (sockfd < 0) {
+		std::cerr << "Error creating socket" << std::endl;
+		return 1;
 	}
 
-    // Socket Values
+    // Set up socket address
     my_addr.sin_family = AF_INET;
 	my_addr.sin_port = htons(servPortNumber);
-	my_addr.sin_addr.s_addr = INADDR_ANY; /* bind to all local interfaces */
+	my_addr.sin_addr.s_addr = INADDR_ANY;
 	bzero(&(my_addr.sin_zero), 8);
 
     // Bind
     if (bind(sockfd, (struct sockaddr *) &my_addr, sizeof(struct sockaddr)) < 0) {
-		perror("bind");
-		exit(1);
+		std::cerr << "Error binding" << std::endl;
+		return 1;
 	}
 
     // Listen
-    if (listen(sockfd, LISTEN_QUEUE) < 0) {
+    if (listen(sockfd, LISTEN_QUEUE_SIZE) < 0) {
 		perror("listen");
-		exit(1);
+        std::cerr << "Error listening for connections" << std::endl;
+		return 1;
 	}
     
     std::cout << "Listening on port " << servPortNumber << std::endl;
@@ -170,7 +285,7 @@ int main(int argc, char *argv[])
         // Accept
 		if ((client_sockfd = accept(sockfd,
         (struct sockaddr *) &their_addr, &sin_size)) < 0) {
-			perror("accept");
+			std::cerr << "Error accepting connection" << std::endl;
 			continue;
 		}
 
