ff92791e716024f1e9c222fbe493238b2750b689
crowderlane001 <122763205+crowderlane001@users.noreply.github.com>
2025-04-27 13:22:13 -0400

    Env fix

--- frontend/app/env.tsx
+++ frontend/app/env.tsx
@@ -1,8 +1,18 @@
 export const COUCHDB_USER: string | undefined = process.env.COUCHDB_USER;
 export const COUCHDB_PASSWORD: string | undefined = process.env.COUCHDB_PASSWORD;
 
-export const COUCHDB_URL_AUTH = process.env.NODE_ENV === "production" ? `http://${COUCHDB_USER}:${COUCHDB_PASSWORD}@database:5984` : `http://${COUCHDB_USER}:${COUCHDB_PASSWORD}@localhost:5984`;
-export const COUCHDB_URL = process.env.NODE_ENV === "production" ? `http://database:5984` : `http://localhost:5984`;
+export const COUCHDB_URL_AUTH = process.env.NODE_ENV === "production"
+  ? `http://${COUCHDB_USER}:${COUCHDB_PASSWORD}@database:5984`
+  : `http://${COUCHDB_USER}:${COUCHDB_PASSWORD}@localhost:5984`;
 
-export const ALGO_URL = process.env.NODE_ENV === "production" ? "http://algorithmic-trader:8000" : "http://localhost:8000";
-export const LLM_URL = process.env.NODE_ENV === "production" ? "http://llm-server:8011" : "http://localhost:8011";x
+export const COUCHDB_URL = process.env.NODE_ENV === "production"
+  ? `http://database:5984`
+  : `http://localhost:5984`;
+
+export const ALGO_URL = process.env.NODE_ENV === "production"
+  ? "http://algorithmic-trader:8000"
+  : "http://localhost:8000";
+
+export const LLM_URL = process.env.NODE_ENV === "production"
+  ? "http://llm-server:8011"
+  : "http://localhost:8011";
